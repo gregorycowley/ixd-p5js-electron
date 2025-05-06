7ba01b29f1b51202adfa4ce364a54382ebb68d21
Gregory Cowley <gregory@cowleystudios.com>
2025-05-06 10:00:46 -0700

    Changes before archive

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 const { app, BrowserWindow } = require('electron');
-
 const path = require('path');
-const url = require('url');
+const express = require('express');
 
 let mainWindow;
+const PORT = 3000;
+
+const url = require('url');
+
+// Start Express server to serve the web content
+const serverApp = express();
+serverApp.use(express.static(path.join(__dirname, 'public')));
+serverApp.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
     useContentSize: true,
   });
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'public', 'index.html'),
-      protocol: 'file:',
-      slashes: true,
-    })
-  );
+  mainWindow.loadURL(`http://localhost:${PORT}`);
+
+  // mainWindow.loadURL(
+  //   url.format({
+  //     pathname: path.join(__dirname, 'public', 'index.html'),
+  //     protocol: 'file:',
+  //     slashes: true,
+  //   })
+  // );
 
   mainWindow.on('closed', function () {
     mainWindow = null;
@@ -39,3 +53,18 @@ app.on('activate', function () {
     createWindow();
   }
 });
+
+// // Create the Electron window
+// app.whenReady().then(() => {
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       contextIsolation: true,
+//     },
+//   });
+
+//   mainWindow.loadURL(`http://localhost:${PORT}`);
+//   mainWindow.on('closed', () => (mainWindow = null));
+// });
